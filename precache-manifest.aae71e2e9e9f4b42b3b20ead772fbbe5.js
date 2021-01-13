5af30c0faad29ad9386e31dc52b13619b687411b
Arpitha Patil <you@example.com>
2021-01-12 22:03:45 -0600

    Updates

---
+++ precache-manifest.aae71e2e9e9f4b42b3b20ead772fbbe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70f7555bf007050da05a",
+    "url": "/portfolio/static/css/main.aeb27868.chunk.css"
+  },
+  {
+    "revision": "70f7555bf007050da05a",
+    "url": "/portfolio/static/js/main.70f7555b.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "408de5e9aaa4cc7ad9eb57f90c6ecf98",
+    "url": "/portfolio/index.html"
+  }
+];
